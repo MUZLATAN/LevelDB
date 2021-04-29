0dea3dc23595965dc294d9473e173b00021f4002
liangjiang <liangjiang@whale.im>
2021-04-29 20:47:02 +0800

    rm cmake-build

--- cmake-build-debug/third_party/benchmark/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/z/CWorkspace/leveldb/third_party/benchmark
-# Build directory: /home/z/CWorkspace/leveldb/cmake-build-debug/third_party/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- cmake-build-debug/third_party/benchmark/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/z/CWorkspace/leveldb/third_party/benchmark/src
-# Build directory: /home/z/CWorkspace/leveldb/cmake-build-debug/third_party/benchmark/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
